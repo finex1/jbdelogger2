121a35bb047ed0e0e40079093c16cf32668156b2
finex1 <finex@youths4youths.com>
2018-11-22 20:14:51 +0000

    Add files via upload

--- customActivity.js
+++ customActivity.js
@@ -123,7 +123,7 @@ define([
         }
 
         currentStep = step;
-alert(currentStep.key);
+alert(onGetTokens);
         $('.step').hide();
 
         switch(currentStep.key) {
@@ -184,7 +184,7 @@ alert(currentStep.key);
         // Journey Builder sends an initial payload with defaults
         // set by this activity's config.json file.  Any property
         // may be overridden as desired.
-      payload.name = onGetTokens;
+      payload.name = name;
 
         payload['arguments'].execute.inArguments = [{ "message": value }];
 
