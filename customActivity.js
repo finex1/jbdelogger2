2820aac5a3e1dfc13b6deaae54ac27f83491e75c
finex1 <finex@youths4youths.com>
2018-11-22 20:27:29 +0000

    Add files via upload

--- customActivity.js
+++ customActivity.js
@@ -32,7 +32,7 @@ define([
 
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
-		printObject('requestTokens');
+		printObject(connection.trigger('requestTokens'));
 
         // Disable the next button if a value isn't selected
         $('#select1').change(function() {
