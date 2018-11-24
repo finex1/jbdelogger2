0d875c288b4b75f255d3a8137a430b578e9cae9c
finex1 <finex@youths4youths.com>
2018-11-24 10:24:11 +0000

    Add files via upload

---
+++ publish/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<?php
+echo "<pre>"; print_r($_POST) ;  echo "</pre>";
+?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Custom Journey Builder Activity</title>
+   
+    
+
+</head>
+<body>
+<div id="step1" class="step">
+
+
+   
+</div>
+
+
+</body>
+</html>
